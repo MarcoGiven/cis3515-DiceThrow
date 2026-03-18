34fb73a87872d30c809311233e44d5cd00f1a44d
MarcoGiven <marcogiven15@gmail.com>
2026-03-18 15:32:37 -0400

    added another fragmentContainer with new dieFragment instance

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -2,10 +2,7 @@ package edu.temple.dicethrow
 
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,12 +13,17 @@ class MainActivity : AppCompatActivity() {
         if(supportFragmentManager.findFragmentById(R.id.fragmentContainerView) == null){
             supportFragmentManager
                 .beginTransaction()
-                .add(R.id.fragmentContainerView, DieFragment.newInstance(20))
+                .add(R.id.fragmentContainerView, DieFragment.newInstance(6))
+                .add(R.id.fragmentContainerView2, DieFragment.newInstance(20))
                 .commit()
         } else {
             findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
                 supportFragmentManager
                     .findFragmentById(R.id.fragmentContainerView)?.run {
+                        (this as DieFragment).throwDie()
+                    }
+                supportFragmentManager
+                    .findFragmentById(R.id.fragmentContainerView2)?.run {
                         (this as DieFragment).throwDie()
                     }
             }
